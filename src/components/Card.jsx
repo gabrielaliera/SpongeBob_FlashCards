8021fa0520fd494af96733b932a775b96b8cef64
gabrielaliera <gliera2990@gmail.com>
2023-03-01 12:03:47 -0800

    fix card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,36 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import React from 'react'
 import './Card.css'
+import House from '../assets/house.jpeg'
 
-const Card = (props) => {
+const Card = ({cards}) => {
+
+    const [currentCard, setCurrentCard] = useState({id:0, name:"Click to Start", image:House});
+
+    const [flip, setFlip] = useState(false);
+
+    const nextCard = () =>{
+        let randomNextCard = Math.floor(Math.random() * cards.length)
+        setCurrentCard(cards[randomNextCard])
+        setFlip(false)
+    };
+    
 
     return(
-        <div className="Card">
-             <p>{props.name}</p>
-             <img src={props.img}/>
-             <button>Left</button>
-             <button>Right</button>
+        <div>
+            <h3>Number of cards:{cards.length}</h3>
+           <div   className={`card ${flip ? "flip" : ""}`} onClick={ () => setCardSide(!cardSide)}>
+                 
+               
+                <div className="front">
+                    <p>{currentCard.name}</p>
+                
+                </div>
+                <div className="back">
+                    <img className="image" src={currentCard.image} alt={`Small image for card ${currentCard.id}`}/>
+                </div>
+            </div>
+           <button onClick={nextCard}>Next</button>
         </div>
     )
 }
