2032082bd5fa246e4629218d5cb0e67dde9cbb1b
gabrielaliera <gliera2990@gmail.com>
2023-03-01 10:52:33 -0800

    Add cardPairs array

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 
 import './App.css'
+import Cards from './components/Card.jsx'
+import SpongeBob from './assets/spongebob.png'
+import Patrick from './assets/patrick.png'
+import Squiward from './assets/squidward.png'
+
 
 const App = () => {
-  const [count, setCount] = useState(0)
+
+  const cardPairs = [
+    {name: "SpongeBob SquarePants", image: SpongeBob },
+    {name: "Patrick Star", image: Patrick },
+    {name: "Squidward", image:Squiward }
+  ]
 
   return (
     <div className="App">
-      
+      <h1>SpongeBob Trivia</h1>
+      <h2>Can you correctly name the following characters?</h2>
+      <h2>Number of cards: {cardPairs.length}</h2>
+      <Cards name= {cardPairs[0].name} img={cardPairs[0].image}/>
     </div>
   )
 }
